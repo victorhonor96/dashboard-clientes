13ae614e6be5c08765b67a5271dcf866246d61f5
victorhonor96 <victor.honor96@gmail.com>
2026-04-28 02:12:35 -0300

    mudando para ploty

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 # título
 st.title("📊 Dashboard de Clientes")
@@ -21,15 +22,21 @@ elif status == "Inativos":
     df = df[df["ativo"] == False]
 
 # gráfico de salários
-st.subheader("💰 Salário por cliente")
+
+
+st.subheader("💰 Salário por cliente (interativo)")
 
 df_ordenado = df.sort_values(by="salario", ascending=False)
 
-fig, ax = plt.subplots()
-ax.bar(df_ordenado["nome"], df_ordenado["salario"])
-plt.xticks(rotation=45)
+fig = px.bar(
+    df_ordenado,
+    x="nome",
+    y="salario",
+    title="Salário por Cliente",
+    text="salario"
+)
 
-st.pyplot(fig)
+st.plotly_chart(fig, use_container_width=True)
 
 # média salarial
 media = df["salario"].mean()
@@ -38,6 +45,16 @@ st.metric("Salário médio", f"{media:.2f}")
 # comparação ativos vs inativos
 st.subheader("📊 Comparação geral")
 
-media_grupo = pd.read_csv("clientes.csv").groupby("ativo")["salario"].mean()
+media_grupo = pd.read_csv("clientes.csv").groupby("ativo")["salario"].mean().reset_index()
 
-st.bar_chart(media_grupo)
+media_grupo["ativo"] = media_grupo["ativo"].map({True: "Ativos", False: "Inativos"})
+
+fig2 = px.bar(
+    media_grupo,
+    x="ativo",
+    y="salario",
+    text="salario",
+    title="Salário médio por status"
+)
+
+st.plotly_chart(fig2, use_container_width=True)
