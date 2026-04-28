8ea8f4133d9abb497a3d37cef3fa6ac64fd7dcd0
victorhonor96 <victor.honor96@gmail.com>
2026-04-28 02:20:16 -0300

    colocando IA

--- app.py
+++ app.py
@@ -3,6 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+# features (entrada)
+X = df[["idade", "ativo"]]
+
+# alvo (o que queremos prever)
+y = df["salario"]
+
+# converter booleano pra número
+X["ativo"] = X["ativo"].astype(int)
+
+# dividir treino/teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# modelo
+modelo = RandomForestRegressor()
+modelo.fit(X_train, y_train)
+
+# precisão
+score = modelo.score(X_test, y_test)
+
+st.metric("Acurácia do modelo", f"{score:.2f}")
+
+
 # título
 st.title("📊 Dashboard de Clientes")
 
@@ -58,3 +83,14 @@ fig2 = px.bar(
 )
 
 st.plotly_chart(fig2, use_container_width=True)
+
+st.subheader("🔮 Prever salário")
+
+idade_input = st.number_input("Idade", 18, 80, 30)
+ativo_input = st.selectbox("Ativo?", ["Sim", "Não"])
+
+ativo_num = 1 if ativo_input == "Sim" else 0
+
+if st.button("Prever salário"):
+    previsao = modelo.predict([[idade_input, ativo_num]])
+    st.success(f"Salário previsto: R$ {previsao[0]:.2f}")
